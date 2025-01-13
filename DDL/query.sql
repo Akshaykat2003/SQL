ae1fb1ce3e37bc81de620a35304e7640d5030319
akshay <akshaykatoch38@gmail.com>
2025-01-14 01:58:38 +0530

    [Akshay] MOD :Done some changes

--- DDL/query.sql
+++ DDL/query.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE
+
 CREATE TABLE
           List of relations
  Schema |   Name   | Type  |  Owner   
@@ -7,7 +8,9 @@ CREATE TABLE
 (1 row)
 
 
+
                                           Table "public.employee"
+
    Column    |         Type          | Collation | Nullable |                    Default                    
 -------------+-----------------------+-----------+----------+-----------------------------------------------
  employee_id | integer               |           | not null | nextval('employee_employee_id_seq'::regclass)
@@ -19,7 +22,8 @@ CREATE TABLE
 
 
 
-ALTER TABLE employee ADD COLUMN department VARCHAR(50);
+-- ALTER TABLE employee ADD COLUMN department VARCHAR(50);
+
                                           Table "public.employee"
    Column    |         Type          | Collation | Nullable |                    Default                    
 -------------+-----------------------+-----------+----------+-----------------------------------------------
@@ -31,8 +35,8 @@ ALTER TABLE employee ADD COLUMN department VARCHAR(50);
 
 
 
-ALTER TABLE
-ALTER TABLE employee ALTER COLUMN salary DROP NOT NULL;
+-- ALTER TABLE
+-- ALTER TABLE employee ALTER COLUMN salary DROP NOT NULL;
 
   
                                           Table "public.employee"
@@ -48,7 +52,8 @@ ALTER TABLE employee ALTER COLUMN salary DROP NOT NULL;
 
 
 
-ALTER TABLE employee RENAME TO staff;
+-- ALTER TABLE employee RENAME TO staff;
+
                                             Table "public.staff"
    Column    |         Type          | Collation | Nullable |                    Default                    
 -------------+-----------------------+-----------+----------+-----------------------------------------------
@@ -61,7 +66,8 @@ ALTER TABLE employee RENAME TO staff;
 
 
 
- ALTER TABLE staff DROP COLUMN department;
+--  ALTER TABLE staff DROP COLUMN department;
+
                                             Table "public.staff"
    Column    |         Type          | Collation | Nullable |                    Default                    
 -------------+-----------------------+-----------+----------+-----------------------------------------------
@@ -74,11 +80,12 @@ ALTER TABLE employee RENAME TO staff;
 
 
 
-CREATE TABLE
-CREATE TABLE departments (
-    department_id SERIAL PRIMARY KEY,
-    department_name VARCHAR(50)
-);
+-- CREATE TABLE
+-- CREATE TABLE departments (
+--     department_id SERIAL PRIMARY KEY,
+--     department_name VARCHAR(50)
+-- );
+
 
                                              Table "public.departments"
      Column      |         Type          | Collation | Nullable |                      Default                       
@@ -88,17 +95,14 @@ CREATE TABLE departments (
 
 
 
-
-
 CREATE TABLE
-
  CREATE DATABASE inventory;
-  \c inventory;
  CREATE TABLE products (
     product_id SERIAL PRIMARY KEY,
     product_name VARCHAR(50),
     price DECIMAL(10, 2)
 );
+
 
 
                                           Table "public.products"
@@ -125,35 +129,28 @@ CREATE TABLE
 
 
 
+-- CREATE TABLE employees (
+--     employee_id SERIAL PRIMARY KEY,
+--     first_name VARCHAR(50),
+--     last_name VARCHAR(50),
+--     salary DECIMAL(10, 2)
+-- );
+-- ALTER TABLE employees ADD COLUMN department VARCHAR(50);
+-- ALTER TABLE employees ALTER COLUMN salary DROP NOT NULL;
+-- ALTER TABLE employees RENAME TO staff;
+-- ALTER TABLE staff DROP COLUMN department;
 
-COMBINED Commands
+-- CREATE TABLE departments (
+--     department_id SERIAL PRIMARY KEY,
+--     department_name VARCHAR(50)
+-- );
+-- CREATE DATABASE inventory;
 
-\o C:/SQLOutputs/output.sql
-CREATE DATABASE company;
-\c company
 
-CREATE TABLE employees (
-    employee_id SERIAL PRIMARY KEY,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    salary DECIMAL(10, 2)
-);
-ALTER TABLE employees ADD COLUMN department VARCHAR(50);
-ALTER TABLE employees ALTER COLUMN salary DROP NOT NULL;
-ALTER TABLE employees RENAME TO staff;
-ALTER TABLE staff DROP COLUMN department;
+-- CREATE TABLE products (
+--     product_id SERIAL PRIMARY KEY,
+--     product_name VARCHAR(50),
+--     price DECIMAL(10, 2)
+-- );
+-- DROP DATABASE inventory;
 
-CREATE TABLE departments (
-    department_id SERIAL PRIMARY KEY,
-    department_name VARCHAR(50)
-);
-CREATE DATABASE inventory;
-\c inventory
-
-CREATE TABLE products (
-    product_id SERIAL PRIMARY KEY,
-    product_name VARCHAR(50),
-    price DECIMAL(10, 2)
-);
-DROP DATABASE inventory;
-\o
